783f86bc09cf2241753c3ceac868d035df743ade
Yulia C <yulka1355@gmail.com>
2025-07-19 11:57:43 +0300

    Complete first exercise

--- 01-about-time.js
+++ 01-about-time.js
@@ -1,1 +1,20 @@
-console.log('Hello');
+import ms from 'ms'
+import fs from 'fs'
+
+
+
+const timeStamps = fs.readFileSync('data/about-time.txt', 'utf8')
+const splitLines = timeStamps.split(/\r\n/)
+
+aboutTime()
+
+function aboutTime() {
+    splitLines.forEach(line => {
+        const numberedDate = parseInt(line)
+        const now = Date.now()
+        const diff = ms(numberedDate, { long: true })
+        console.log(diff, 'ago')
+    })
+
+}
+
